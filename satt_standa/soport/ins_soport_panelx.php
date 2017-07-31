02a13bd7db1a9220c38b56fb31916a286f0d9eae
andres torres <andres.torres@intrared.net>
2017-07-31 12:44:45 -0500

    ajuste en el modulo de mesa de apoyo cuando el usuario tiene tildes

--- satt_standa/soport/ins_soport_panelx.php
+++ satt_standa/soport/ins_soport_panelx.php
@@ -25,7 +25,54 @@ class SpgPanelControl
 		$this->cCodAplica = $ca;
 		$this->Main();
 	}
+
+		function limpiar_caracteres_especiales($string) {
+	 
+
+	    $string = mb_convert_encoding($string, 'UTF-8','');
+	 
+	    $string = str_replace(
+	        array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä'),
+	        array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A'),
+	        $string
+	    );
+	 
+	    $string = str_replace(
+	        array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'),
+	        array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'),
+	        $string
+	    );
+	 
+	    $string = str_replace(
+	        array('í', 'ì', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'),
+	        array('i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'),
+	        $string
+	    );
+	 
+	    $string = str_replace(
+	        array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'),
+	        array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'),
+	        $string
+	    );
+	 
+	    $string = str_replace(
+	        array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'),
+	        array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'),
+	        $string
+	    );
+	 
+	    $string = str_replace(
+	        array('ñ', 'Ñ', 'ç', 'Ç'),
+	        array('n', 'N', 'c', 'C'),
+	        $string
+	    );
+	  
+	     
+	    return $string;
+	}
+	
 	function setArrDatSol(){
+		$_SESSION["datos_usuario"]['nom_usuari'] = $this->limpiar_caracteres_especiales( $_SESSION['datos_usuario']['nom_usuari'] );
 		@include_once( '../' . DIR_APLICA_CENTRAL . '/lib/general/functions.inc' );
 		@include_once( '../' . DIR_APLICA_CENTRAL . '/inform/class_despac_trans3.php' );
 		$this->cDespac = new Despac( $this->cConexion, $this->cUsuario, $this->cCodAplica);
