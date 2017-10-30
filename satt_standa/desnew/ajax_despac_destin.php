144cb45b100c7ab4919c1e8a563ecfb1a0980e22
nliberato <nelson.liberato@intrared.net>
2017-10-30 10:54:43 -0500

    Se agrega nuevo estado a la lista de destinatarios, registados por el cron destinatarios

--- satt_standa/desnew/ajax_despac_destin.php
+++ satt_standa/desnew/ajax_despac_destin.php
@@ -19,6 +19,8 @@ class AjaxDespacDestin {
         $this->ind_estado[1][1] = 'PENDIENTE';
         $this->ind_estado[2][0] = 'ACTUALIZADO';
         $this->ind_estado[2][1] = 'ACTUALIZADO';
+	$this->ind_estado[3][0] = 'REGISTRADO SISTEMA';
+	$this->ind_estado[3][1] = 'REGISTRADO SISTEMA';
         $this->ind_destin = "";
         $this->num_docume = "";
 
@@ -46,9 +48,6 @@ class AjaxDespacDestin {
 
         $consulta = new Consulta($mSelect, $this->conexion);
         $_DESTIN = $consulta->ret_matriz();
-
-
-
 
 
 
@@ -719,7 +718,7 @@ class AjaxDespacDestin {
             default:
                         $mSql = "SELECT w.num_despac, w.num_desext, w.ind_modifi, if(w.num_destin = 0, w.num_destin,COUNT(w.num_destin)) AS num_client, w.num_placax, w.cod_manifi, w.fec_despac, w.nom_tipdes, w.nom_ciuori, w.nom_ciudes 
                                FROM (
-                                SELECT a.num_despac, IF( z.num_desext IS NOT NULL , z.num_desext,  'N/A' ) as num_desext , IF( f.ind_modifi =  '1', 'ACTUALIZADO', 'PENDIENTE' ) as ind_modifi, count(f.num_destin) AS num_destin, b.num_placax, a.cod_manifi, a.fec_despac, c.nom_tipdes, d.nom_ciudad AS nom_ciuori, e.nom_ciudad AS nom_ciudes
+                                SELECT a.num_despac, IF( z.num_desext IS NOT NULL , z.num_desext,  'N/A' ) as num_desext , IF( f.ind_modifi =  '1', 'ACTUALIZADO', IF( f.ind_modifi = '3', 'REGISTRADO SISTEMA', 'PENDIENTE') ) as ind_modifi, count(f.num_destin) AS num_destin, b.num_placax, a.cod_manifi, a.fec_despac, c.nom_tipdes, d.nom_ciudad AS nom_ciuori, e.nom_ciudad AS nom_ciudes
                                   FROM satt_faro.tab_despac_despac a
                              LEFT JOIN satt_faro.tab_despac_sisext z ON a.num_despac = z.num_despac
                              LEFT JOIN satt_faro.tab_despac_destin f ON a.num_despac = f.num_despac, satt_faro.tab_despac_vehige b, satt_faro.tab_genera_tipdes c, satt_faro.tab_genera_ciudad d, satt_faro.tab_genera_ciudad e
