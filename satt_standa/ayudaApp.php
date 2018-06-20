cda4450aa7ef9a5f5ce8a8b346dce93321247b98
Hector Leal - Pruebas <hleal@avansatgl.intrared.net>
2018-06-20 11:26:55 -0500

    se versiona nuevamente el manual de la app para faro

---
+++ satt_standa/ayudaApp.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$URL = "https://".$_SERVER["HTTP_HOST"]."/ap/satt_standa/manual/Instructivo Aplicación Avansat Móvil.pdf";
+?>
+    <head>
+        <title> Manual Aplicación Móvil AVANSAT</title>
+    </head>
+    <body style="overflow:hidden">
+        <?php
+            echo "<iframe src='".$URL."' width='100%' height='90%' name='manual' id='manual'></iframe>";
+            echo "<script>document.getElementById('manual').height=(screen.height)*0.85;</script>";
+        ?>
+    </body>
+</html>
