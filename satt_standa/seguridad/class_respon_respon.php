84ee49fc3569aff2e2fde5b23c0eb47f563e0152
cristian.torres <cristian.torres@eltransporte.org>
2021-10-15 07:50:48 -0500

    [REQ_373150] Ajuste modulo responsables

--- satt_standa/seguridad/class_respon_respon.php
+++ satt_standa/seguridad/class_respon_respon.php
@@ -97,11 +97,11 @@ class responsable
 			$mSql = "INSERT INTO ".BASE_DATOS.".tab_genera_respon 
 						( cod_respon, nom_respon, ind_activo, 
 						  jso_bandej, jso_encabe, jso_plarut, 
-						  jso_infcal, jso_notifi, jso_contac, jso_partic, jso_obsgen, edt_gpsxxx, jso_estseg, usr_creaci, fec_creaci ) 
+						  jso_infcal, jso_notifi, jso_contac, jso_partic, jso_obsgen, edt_gpsxxx, jso_progra, jso_estseg, usr_creaci, fec_creaci ) 
 					VALUES 
 						( '".($mCodRespon[0]+1)."', '".$_REQUEST['nom_respon']."', '".$_REQUEST['ind_activo']."', 
 						  '".json_encode($_REQUEST['jso_bandej'])."', '".json_encode($_REQUEST['jso_encabe'])."', '".json_encode($_REQUEST['jso_plarut'])."', 
-						  '".json_encode($_REQUEST['jso_infcal'])."', '".json_encode($_REQUEST['jso_notifi'])."', '".json_encode($_REQUEST['jso_contac'])."', '".json_encode($_REQUEST['jso_partic'])."','".json_encode($_REQUEST['jso_obsgen'])."','".json_encode($_REQUEST['edt_gpsxxx'])."' , 
+						  '".json_encode($_REQUEST['jso_infcal'])."', '".json_encode($_REQUEST['jso_notifi'])."', '".json_encode($_REQUEST['jso_contac'])."', '".json_encode($_REQUEST['jso_partic'])."','".json_encode($_REQUEST['jso_obsgen'])."','".json_encode($_REQUEST['edt_gpsxxx'])."' ,'".json_encode($_REQUEST['jso_progra'])."' ,
 						  '".json_encode($_REQUEST['jso_estseg'])."','".$_SESSION['datos_usuario']['cod_usuari']."', NOW() ) 	
 					";
 			$mConsult = new Consulta($mSql, self::$cConexion);
@@ -120,6 +120,7 @@ class responsable
 													jso_contac = '".json_encode($_REQUEST['jso_contac'])."',
 													jso_partic = '".json_encode($_REQUEST['jso_partic'])."',
 													edt_gpsxxx = '".json_encode($_REQUEST['edt_gpsxxx'])."',
+													jso_progra = '".json_encode($_REQUEST['jso_progra'])."',
 													jso_obsgen = '".json_encode($_REQUEST['jso_obsgen'])."',
 													jso_estseg = '".json_encode($_REQUEST['jso_estseg'])."'," : NULL )."
 						fec_modifi = NOW() 
@@ -150,7 +151,7 @@ class responsable
 	 */
 	private function edicionRespon()
 	{
-		$mSql = "SELECT jso_bandej, jso_encabe, jso_plarut, jso_infcal, jso_notifi, jso_contac, jso_partic, jso_obsgen, edt_gpsxxx, jso_estseg
+		$mSql = "SELECT jso_bandej, jso_encabe, jso_plarut, jso_infcal, jso_notifi, jso_contac, jso_partic, jso_obsgen, edt_gpsxxx, jso_progra, jso_estseg
 				   FROM ".BASE_DATOS.".tab_genera_respon 
 				  WHERE cod_respon = '".$_REQUEST['cod_respon']."' ";
 		$mConsult = new Consulta($mSql, self::$cConexion);
@@ -164,12 +165,13 @@ class responsable
 		$mData['jso_notifi'] = json_decode($mData['jso_notifi']);
 		$mData['jso_contac'] = json_decode($mData['jso_contac']);
 		$mData['edt_gpsxxx'] = json_decode($mData['edt_gpsxxx']);
+		$mData['jso_progra'] = json_decode($mData['jso_progra']);
 		$mData['jso_partic'] = json_decode($mData['jso_partic']);
 		$mData['jso_obsgen'] = json_decode($mData['jso_obsgen']);
 		$mData['jso_estseg'] = json_decode($mData['jso_estseg']);
 
 		self::style();
-		$mCategoria = array("jso_bandej"=>'Bandeja', "jso_encabe"=>'Encabezado', "jso_plarut"=>'Plan de Ruta', "jso_infcal"=>'Informe Auditorias General', "jso_notifi"=>'de notificaciones', "jso_contac"=>'contactos', "jso_partic"=>'particularidades', "jso_obsgen"=>'Observacion general', "edt_gpsxxx"=>'Editar GPS', "jso_estseg"=>"Estudio de seguridad");
+		$mCategoria = array("jso_bandej"=>'Bandeja', "jso_encabe"=>'Encabezado', "jso_plarut"=>'Plan de Ruta', "jso_infcal"=>'Informe Auditorias General', "jso_notifi"=>'de notificaciones', "jso_contac"=>'contactos', "jso_partic"=>'particularidades', "jso_obsgen"=>'Observacion general', "edt_gpsxxx"=>'Editar GPS', "jso_progra"=>'Configuracion Programacion', "jso_estseg"=>"Estudio de seguridad");
 		$mChecked = $_REQUEST['ind_activo'] == '1' ? 'true' : 'false';
 
 		$mHtml = new Formlib(2);
@@ -433,6 +435,16 @@ class responsable
 				$mArray[1] = array( "dat_gpsxxx"=>array("ind_visibl"=>1, "sub"=>array())
 								);
 				break;
+			case 'jso_progra':
+				$mArray[0] = array( "dat_progra"=>array("name"=>"Visualizar", "sub"=>array() ),
+									"dat_elimin"=>array("name"=>"Eliminar", "sub"=>array() )
+									);
+				$mArray[1] = array( "dat_progra"=>array("ind_visibl"=>1, "sub"=>array()),
+									"dat_elimim"=>array("ind_visibl"=>1, "sub"=>array()),
+								);
+				break;
+
+				
 			case 'jso_partic':
 				$mArray[0] = array( "dat_partic"=>array("name"=>"Visualizar", "sub"=>array() )
 								  );
