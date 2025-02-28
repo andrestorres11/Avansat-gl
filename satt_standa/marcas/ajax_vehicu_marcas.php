6a0504f711b40bbb9dcb459cce843490f4b65b62
jsanchez <jesus.sanchez@grupooet.com>
2025-02-28 08:31:07 -0500

    [REQ_443999_V5] - Ajuste de libreria swal y caracteres especiales

--- satt_standa/marcas/ajax_vehicu_marcas.php
+++ satt_standa/marcas/ajax_vehicu_marcas.php
@@ -145,6 +145,27 @@ class AjaxVehicuMarcas
   
         echo json_encode($output, JSON_UNESCAPED_UNICODE);
     }
+
+    function eliminarAcentos($cadena){
+		
+		$caracteres = array(
+            // Vocales acentuadas
+            'Á' => 'A', 'À' => 'A', 'Â' => 'A', 'Ä' => 'A', 'á' => 'a', 'à' => 'a', 'â' => 'a', 'ä' => 'a',
+            'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+            'Í' => 'I', 'Ì' => 'I', 'Î' => 'I', 'Ï' => 'I', 'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i',
+            'Ó' => 'O', 'Ò' => 'O', 'Ô' => 'O', 'Ö' => 'O', 'ó' => 'o', 'ò' => 'o', 'ô' => 'o', 'ö' => 'o',
+            'Ú' => 'U', 'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U', 'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u',
+            // Otros caracteres especiales
+            'Ñ' => 'N', 'ñ' => 'n', 'Ç' => 'C', 'ç' => 'c', 'ß' => 'ss',
+            // Caracteres adicionales
+            'İ' => 'Y', 'ı' => 'y', 'ÿ' => 'y', '*' => '',
+        );
+    
+        $cadena = strtr($cadena, $caracteres);
+        $cadena = preg_replace('/[\x{FFFD}]/u', '', $cadena);
+    
+        return $cadena;
+	}
    
 
 }
